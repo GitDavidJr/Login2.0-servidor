470d07bbfc7c25173d3adea2b5c137df69e14193
David Júnior <davidjunior051204@gmail.com>
2025-03-14 18:11:37 -0300

    feat: Remove unused password hashing utility

--- src/utils/hash.utils.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt"
-
-const saltRounds = 10;
-
-//console.log( await bcrypt.hash("david1234", saltRounds))
-
-export const comparePassword = async (password, hash) => {
-    return await bcrypt.compare(password, hash)
-}
